d271636351ada2fcca99a518f041424f67d64b2d
Luis Eduardo Herrera Gonzalez <u202218227@upc.edu.pe>
2024-05-23 20:38:15 -0500

    fixes: recombined the snake case strategy with its true name

--- src/main/java/com/deltatech/diligencetech/platform/shared/infrastructure/persistence/jpa/configuration/strategy/SnakeCasePhysicalNamingStrategy.java
+++ src/main/java/com/deltatech/diligencetech/platform/shared/infrastructure/persistence/jpa/configuration/strategy/SnakeCasePhysicalNamingStrategy.java
@@ -6,7 +6,7 @@ import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 
 import static io.github.encryptorcode.pluralize.Pluralize.pluralize;
 
-public class xd implements PhysicalNamingStrategy {
+public class SnakeCasePhysicalNamingStrategy implements PhysicalNamingStrategy {
 
     @Override
     public Identifier toPhysicalCatalogName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
