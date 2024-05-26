3124260c2a5dac2a08e25dc3ba68a7c58ebd7b62
Julio Elsner <u202111654@upc.edu.pe>
2024-05-26 17:12:35 -0500

    feature: created Agent & Notification Aggregates

--- src/main/java/com/deltatech/diligencetech/platform/shared/domain/model/aggregates/Agent.java
+++ src/main/java/com/deltatech/diligencetech/platform/shared/domain/model/aggregates/Agent.java
@@ -1,11 +1,51 @@
-package com.deltatech.diligencetech.platform.duediligence.shared.aggregates;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+package com.deltatech.diligencetech.platform.shared.domain.model.aggregates;
+import com.deltatech.diligencetech.platform.shared.domain.model.commands.CreateAgentCommand;
+import jakarta.persistence.*;
+import lombok.Getter;
 import org.springframework.data.domain.AbstractAggregateRoot;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Agent extends AbstractAggregateRoot<Agent> {
+
   @Id
+  @Getter
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Getter
+  @Column(nullable = false)
+  private String agentId;
+
+  @Getter
+  @Column (nullable = false)
+  private String username;
+
+  @Getter
+  @Column (nullable = false)
+  private String password;
+
+  @Getter
+  @Column (nullable = false)
+  private String email;
+
+
+  public Agent() {
+  }
+
+
+  protected Agent( String agentId, String username, String password, String email){
+    this.agentId = agentId;
+    this.username = username;
+    this.password = password;
+    this.email = email;
+  }
+
+  public Agent(CreateAgentCommand command){
+    this.agentId = command.agentId();
+    this.username = command.username();
+    this.password = command.password();
+    this.email = command.email();
+  }
 }
--- src/main/java/com/deltatech/diligencetech/platform/shared/domain/model/aggregates/Notification.java
+++ src/main/java/com/deltatech/diligencetech/platform/shared/domain/model/aggregates/Notification.java
@@ -1,20 +1,26 @@
-package com.deltatech.diligencetech.platform.duediligence.domain.model.aggregates;
+package com.deltatech.diligencetech.platform.shared.domain.model.aggregates;
 
-import com.deltatech.diligencetech.platform.duediligence.domain.model.commands.CreateNotificationCommand;
+import com.deltatech.diligencetech.platform.shared.domain.model.commands.CreateNotificationCommand;
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.aspectj.weaver.ast.Not;
+import org.springframework.data.domain.AbstractAggregateRoot;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-public class Notification {
+public class Notification extends AbstractAggregateRoot<Notification> {
 
   @Id
   @Getter
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Getter
+  @Column(nullable = false)
+  private String username;
 
   @Getter
   @Column(nullable = false)
@@ -26,20 +32,23 @@ public class Notification {
 
   @Getter
   @Column(nullable = false)
-  private Date createdAt;
+  private String createdAt;
 
 
   public Notification(){
   }
 
 
-  public Notification( String type, String content, Date createdAt){
+  public Notification(String username, String type, String content, Date createdAt){
+    this.username = username;
     this.type = type;
     this.content = content;
-    this.createdAt = createdAt;
+    this.createdAt = String.valueOf(createdAt);
+
   }
 
   public Notification(CreateNotificationCommand command){
+    this.type = command.username();
     this.type = command.type();
     this.content = command.content();
     this.createdAt = command.createdAt();
