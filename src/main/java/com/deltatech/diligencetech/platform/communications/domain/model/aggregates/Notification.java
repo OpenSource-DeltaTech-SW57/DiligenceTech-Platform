2239f2c5e60e9eac8050bf0d56b3307d72cafe50
Oscar Nathaniel Garayar Mori <u202014115@upc.edu.pe>
2024-06-04 23:36:59 -0500

    feat: Implemented Notification endpoint.

--- src/main/java/com/deltatech/diligencetech/platform/communications/domain/model/aggregates/Notification.java
+++ src/main/java/com/deltatech/diligencetech/platform/communications/domain/model/aggregates/Notification.java
@@ -1,5 +1,6 @@
 package com.deltatech.diligencetech.platform.communications.domain.model.aggregates;
 
+import com.deltatech.diligencetech.platform.communications.domain.model.commands.CreateNotificationCommand;
 import com.deltatech.diligencetech.platform.communications.domain.model.valueobjects.NotificationData;
 import com.deltatech.diligencetech.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 import com.deltatech.diligencetech.platform.shared.domain.model.entities.AuditableModel;
@@ -15,5 +16,9 @@ public class Notification extends AuditableAbstractAggregateRoot<Notification> {
   private NotificationData notificationData;
 
   public Notification() { }
+
+  public Notification(CreateNotificationCommand command) {
+    this.notificationData = new NotificationData(command.type(),command.content());
+  }
 
 }
