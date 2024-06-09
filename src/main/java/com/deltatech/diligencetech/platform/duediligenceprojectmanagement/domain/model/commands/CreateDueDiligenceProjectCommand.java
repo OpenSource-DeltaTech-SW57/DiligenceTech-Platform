6a4437ab953652ebb99d4a07cc9c0bae64644da2
Luis Eduardo Herrera Gonzalez <U202218227@upc.edu.pe>
2024-06-09 15:25:01 -0500

    fixes: merge conflicts fixed.

--- src/main/java/com/deltatech/diligencetech/platform/duediligenceprojectmanagement/domain/model/commands/CreateDueDiligenceProjectCommand.java
+++
@@ -1,4 +0,0 @@
-package com.deltetech.diligencetech.platform.duediligenceprojectmanagement.domain.model.commands;
-
-public record CreateDueDiligenceProjectCommand(String projectFullName, String managerFirstName, String managerLastName, String startDate, String endDate, Float budget, Long progress, String status) {
-}
