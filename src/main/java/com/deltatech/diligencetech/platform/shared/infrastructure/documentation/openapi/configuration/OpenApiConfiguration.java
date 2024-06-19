ed84b83046f8eaa984b90bac64359b77b58a3c99
Luis Eduardo Herrera Gonzalez <u202218227@upc.edu.pe>
2024-06-19 16:34:09 -0500

    feat(iam): added open api documentation configuration parameters in order to support jwt based authorization.

--- src/main/java/com/deltatech/diligencetech/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/deltatech/diligencetech/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -1,9 +1,12 @@
 package com.deltatech.diligencetech.platform.shared.infrastructure.documentation.openapi.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,6 +26,22 @@ public class OpenApiConfiguration {
                 .externalDocs(new ExternalDocumentation()
                         .description("DELTATECH DiligenceTech Platform Documentation")
                         .url("https://github.com/OpenSource-DeltaTech-SW57/DiligenceTech-Platform"));
+
+        // Add security scheme
+
+        final String securitySchemeName = "bearerAuth";
+
+        openApi.addSecurityItem(new SecurityRequirement()
+                .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+
+        // Return OpenApi configuration object with all the settings
         return openApi;
     }
 }
